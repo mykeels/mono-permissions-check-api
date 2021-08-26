7df4895bcb1c3754bdf2ed01426afbaecd020477
mykeels <mykehell123@gmail.com>
2021-08-26 12:01:11 +0100

    feat: refine search

--- src/banks/bank.controller.ts
+++ src/banks/bank.controller.ts
@@ -19,18 +19,25 @@ export default class BankController extends BaseController {
     @queryParam("search") search: string
   ) {
     try {
-      const threshold = 0.5;
+      const threshold = 0.4;
       this.handleSuccess(
         req,
         res,
-        banks
-          .map((bank) => ({
-            ...bank,
-            matches: search
-              ? compare(bank.name.toLowerCase(), search.toLowerCase())
-              : 1,
-          }))
-          .filter(({ matches }) => matches >= threshold)
+        search?.length === 1
+          ? banks.filter((bank) =>
+              bank.name?.toLowerCase().includes(search?.toLowerCase())
+            )
+          : banks
+              .map((bank) => ({
+                ...bank,
+                matches: search
+                  ? compare(bank.name.toLowerCase(), search.toLowerCase())
+                  : 1,
+              }))
+              .sort((a, b) =>
+                a.matches > b.matches ? -1 : a.matches < b.matches ? 1 : 0
+              )
+              .filter(({ matches }) => matches >= threshold)
       );
     } catch (err) {
       this.handleError(req, res, err);
